448bb6c821018039be87392aae84ea6b2c7698ad
이영현 <hong.git@yahoo.com>
2026-05-05 19:55:09 +0900

    실습2

--- config/config.exs
+++ config/config.exs
@@ -73,6 +73,10 @@ config :logger, :default_formatter,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Disable symlink warning on Windows
+config :phoenix_live_view, :colocated_js,
+  disable_symlink_warning: true
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
